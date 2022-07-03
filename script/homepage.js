5bb99709c171a3ebc816024651d647e0ce29de38
N-ascent <N-ascent@users.noreply.github.com>
2022-07-02 20:23:04 -0700

    (js) Added unit conversion links

--- script/homepage.js
+++ script/homepage.js
@@ -40,12 +40,12 @@ function searchTemp(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#country").innerHTML = response.data.sys.country;
   
-  let max = Math.round(response.data.main.temp_max);
-  let min = Math.round(response.data.main.temp_min);
+  max = response.data.main.temp_max;
+  min = response.data.main.temp_min;
   document.querySelector(
     ".day-temperature"
-    ).innerHTML = `<srtong><b> ${max}° </b></strong>`;
-    document.querySelector(".night-temperature").innerHTML = `/ ${min}°`;
+    ).innerHTML = `<b> ${Math.round(max)}° </b>`;
+    document.querySelector(".night-temperature").innerHTML = `/ ${Math.round(min)}°`;
     
     document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
@@ -57,85 +57,96 @@ function searchTemp(response) {
       document.querySelector("h2").innerHTML = time(response.data.dt * 1000);
       document.querySelector(
         "#icon"
-      ).setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+        ).setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+        
+      }
+      
+      function auto(city) {
+        let key = "727f8c7634f34b68e4f814522083ca30";
+        let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+        axios.get(`${apiurl}`).then(searchTemp);
+      }
+      
+      function search(event) {
+        event.preventDefault();
+        let city = document.querySelector("#enter-city").value;
+        auto(city);
+      }
+      
+      function noon(now) {
+        if (now.getHours() > 11) {
+          return `p.m.`;
+        } else {
+          return `a.m.`;
+        }
+      }
+      
+      
+      function returnDate(now) {
+        let days = [
+          `Sunday`,
+          `Monday`,
+          `Tuesday`,
+          `Wednesday`,
+          `Thursday`,
+          `Friday`,
+          `Satuday`,
+        ];
+        let months = [
+          `January`,
+          `February`,
+          `March`,
+          `April`,
+          `May`,
+          `June`,
+          `July`,
+          `August`,
+          `September`,
+          `October`,
+          `November`,
+          `December`,
+        ];
+        
+        return `${days[now.getDay()]}, ${
+          months[now.getMonth()]
+        } ${now.getDate()} ${now.getFullYear()}`;
+      }
+      
+      function usa(event) {
+        event.preventDefault();
+        
+        let dayFahrenheitTemp = (max * 9) / 5 + 32
+        let nightFahrenheitTemp = (min * 9) / 5 + 32
+        document.querySelector(
+          ".day-temperature"
+          ).innerHTML = `<b> ${Math.round(dayFahrenheitTemp)}° </b>`;
+          document.querySelector(".night-temperature").innerHTML = `/ ${Math.round(
+            nightFahrenheitTemp
+            )}°`;
+          }
+          
+          function canada(event) {
+            event.preventDefault();
+            let dayFahrenheitTemp = max
+            let nightFahrenheitTemp = min
+            document.querySelector(
+              ".day-temperature"
+            ).innerHTML = `<b> ${Math.round(
+              dayFahrenheitTemp
+            )}° </b>`;
+            document.querySelector(
+              ".night-temperature"
+            ).innerHTML = `/ ${Math.round(nightFahrenheitTemp)}°`;
+          }
+          
+          function showPosition(position) {
+            let lat = position.coords.latitude;
+           let lon = position.coords.longitude;
+           let key = `727f8c7634f34b68e4f814522083ca30`;
+          let apiurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 
-}
-
-function auto(city) {
-  let key = "727f8c7634f34b68e4f814522083ca30";
-  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  axios.get(`${apiurl}`).then(searchTemp);
-}
-
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#enter-city").value;
-  auto(city);
-}
-
-function noon(now) {
-  if (now.getHours() > 11) {
-    return `p.m.`;
-  } else {
-    return `a.m.`;
-  }
-}
-
-
-function returnDate(now) {
-  let days = [
-    `Sunday`,
-    `Monday`,
-    `Tuesday`,
-    `Wednesday`,
-    `Thursday`,
-    `Friday`,
-    `Satuday`,
-  ];
-  let months = [
-    `January`,
-    `February`,
-    `March`,
-    `April`,
-    `May`,
-    `June`,
-    `July`,
-    `August`,
-    `September`,
-    `October`,
-    `November`,
-    `December`,
-  ];
-
-  return `${days[now.getDay()]}, ${
-    months[now.getMonth()]
-  } ${now.getDate()} ${now.getFullYear()}`;
-}
-
-function usa(event) {
-  event.preventDefault();
-  let day = document.querySelector(".day-temperature");
-  let night = document.querySelector(".night-temperature");
-  day.innerHTML = `<srtong><b> 70° </b></strong>`;
-  night.innerHTML = `| 66°`;
-}
-
-function canada(event) {
-  event.preventDefault();
-  let day = document.querySelector(".day-temperature");
-  let night = document.querySelector(".night-temperature");
-  day.innerHTML = `<srtong><b> 21° </b></strong>`;
-  night.innerHTML = `| 19°`;
-}
-
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let key = `727f8c7634f34b68e4f814522083ca30`;
-  let apiurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
-
-  axios.get(`${apiurl}`).then(showTemp);
-}
+          axios.get(`${apiurl}`).then(showTemp);
+        }
 
 let now = new Date();
 
